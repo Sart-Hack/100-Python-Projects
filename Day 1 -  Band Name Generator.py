94514a684f784aa19b5160edd2f853810aba67a3
Sarthak124 <sarthakgupta124@gmail.com>
2022-06-26 22:01:04 +0530

    Day 1 Project - Band Name Generator

---
+++ Day 1 -  Band Name Generator.py
@@ -0,0 +1,8 @@
+# This is day 1 project for the 100 Days of Python Course
+
+print("Welcome to the Band Name Generator \n")
+
+city_name = input("What's the name of the city that you grew up in? \n")
+pet_name = input("What's your pet name? \n")
+
+print(f"Your band name is: {city_name} {pet_name}.")
