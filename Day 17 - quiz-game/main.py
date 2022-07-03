d52bba4b996aa1fa65d571f43183ce7c9cf58fa2
Sarthak124 <sarthakgupta124@gmail.com>
2022-07-03 12:20:10 +0530

    Day 17 Project - OOP coffee machine

--- Day 17 - quiz-game/main.py
+++ Day 17 - quiz-game/main.py
@@ -2,7 +2,8 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
-question_bank = [Question(question_entry["text"], question_entry["answer"]) for question_entry in question_data]
+question_bank = [Question(question_entry["question"], question_entry["correct_answer"])
+                 for question_entry in question_data["results"]]
 
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
